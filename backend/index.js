a82b48763ea3049e82440d3c905438c697352174
erny <e.fava13@gmail.com>
2024-05-02 17:26:53 -0400

    INICIO

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+});
+
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+});
