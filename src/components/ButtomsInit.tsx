16fa4ce32267efeeb712b319dffb532698bd77bd
erny <e.fava13@gmail.com>
2024-05-18 17:35:46 -0400

    iconos

--- src/components/ButtomsInit.tsx
+++ src/components/ButtomsInit.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { FaUserLarge } from "react-icons/fa6"
+import { IoNutrition } from "react-icons/io5"
+import { IoMdPhotos } from "react-icons/io"
+import { IoIosChatboxes } from "react-icons/io"
 
 const BotonesInit = () => {
   return (
@@ -7,41 +10,29 @@ const BotonesInit = () => {
             
           <div className="flex flex-row cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100">
             <div className="text-center font-medium text-gray-500">Actualizar medidas</div>
-            <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2">
-              <svg className="inline h-5 w-5 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-              </svg>
+            <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-red-700 text-white p-2">
+              <FaUserLarge />
             </div>
           </div>
 
           <div className="flex flex-row cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100">
             <div className="text-center font-medium text-gray-500">Registros de nutrición</div>
-            <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2">
-              <svg className="inline h-5 w-5 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-              </svg>
+            <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-red-700 text-white p-2">
+              <IoNutrition />
             </div>
           </div>
 
           <div className="flex flex-row cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100">
             <div className="text-center font-medium text-gray-500">Agregar fotos de progreso</div>
-            <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2">
-              <svg className="inline h-5 w-5 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-              </svg>
+            <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-red-700 text-white p-2">
+              <IoMdPhotos />
             </div>
           </div>
 
           <div className="flex flex-row cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100">
             <div className="text-center font-medium text-gray-500">Comenzar chat</div>
-            <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2">
-              <svg className="inline h-5 w-5 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-              </svg>
+            <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-red-700 text-white p-2">
+              <IoIosChatboxes />
             </div>
           </div>
 
