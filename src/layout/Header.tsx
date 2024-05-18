18ccfba31b97a3950bc426f7705d09d13db28a20
erny <e.fava13@gmail.com>
2024-05-18 17:29:27 -0400

    pagina programs

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Button, Navbar } from "flowbite-react"
 import { HiMenuAlt2 } from "react-icons/hi"
 import Menu from './Menu'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 
@@ -11,9 +12,11 @@ const Header = () => {
   return (
     <>
        <Navbar className='bg-[#C72414] rounded-none' fluid rounded>
-        <Navbar.Brand href="#">
-          <span className="self-center whitespace-nowrap text-md text-white font-semibold font-['stencilla']">VictorFisioFitness</span>
-        </Navbar.Brand>
+        <Link to={'/'}>
+          <Navbar.Brand href="#">
+            <span className="self-center whitespace-nowrap text-md text-white font-semibold font-['stencilla']">VictorFisioFitness</span>
+          </Navbar.Brand>
+        </Link>
         <div className="flex md:order-2">
           <div className="flex items-center justify-center">
             <Button id='menu' color="white" onClick={() => setIsOpen(true)}>
